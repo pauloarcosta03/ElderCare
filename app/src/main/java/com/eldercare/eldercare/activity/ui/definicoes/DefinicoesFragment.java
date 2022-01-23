6e913540754895ebeee29fdb29c2818caa52e214
pauloarcosta03 <pauloarcosta03@gmail.com>
2022-01-23 15:37:38 +0000

    Definições censuradas

--- app/src/main/java/com/eldercare/eldercare/activity/ui/definicoes/DefinicoesFragment.java
+++ app/src/main/java/com/eldercare/eldercare/activity/ui/definicoes/DefinicoesFragment.java
@@ -13,12 +13,25 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.eldercare.eldercare.R;
+import com.eldercare.eldercare.config.ConfiguracaoFirebase;
+import com.eldercare.eldercare.helper.Base64Custom;
+import com.eldercare.eldercare.model.Utilizador;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class DefinicoesFragment extends Fragment {
 
     private Button ButtonAddPaciente;
     private Button ButtonRemConta;
+
+    private FirebaseAuth autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+    private DatabaseReference firebaseRef = ConfiguracaoFirebase.getFirebaseRef();
+    private DatabaseReference utilizadorRef;
+    private String permicao;
 
     public DefinicoesFragment() {
         // Required empty public constructor
@@ -43,6 +56,32 @@ public class DefinicoesFragment extends Fragment {
 
         ButtonAddPaciente = view.findViewById(R.id.ButtonAddPaciente);
         ButtonRemConta = view.findViewById(R.id.ButtonRemConta);
+
+        //Ver se o utilizador tem permições
+        String emailUtilizador = autenticacao.getCurrentUser().getEmail();
+        String idUtilizador = Base64Custom.codificarBase64(emailUtilizador);
+
+        utilizadorRef = firebaseRef.child("utilizadores")
+                .child(idUtilizador);
+
+        utilizadorRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                Utilizador utilizador = snapshot.getValue(Utilizador.class);
+
+                if(utilizador.getTipo().equals("p")){
+                    ButtonAddPaciente.setVisibility(View.INVISIBLE);
+                    ButtonRemConta.setVisibility(View.INVISIBLE);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         ButtonAddPaciente.setOnClickListener(new View.OnClickListener() {
             @Override
