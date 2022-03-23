b8fbd5eab834dd1b02a13d38c05e4fc0bc230175
pauloarcosta03 <pauloarcosta03@gmail.com>
2022-03-23 22:57:13 +0000

    notificações a cada alteração do cuidador

--- app/src/main/java/com/eldercare/eldercare/model/Contacto.java
+++ app/src/main/java/com/eldercare/eldercare/model/Contacto.java
@@ -1,7 +1,10 @@
 package com.eldercare.eldercare.model;
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
+import com.eldercare.eldercare.api.NotificacaoService;
 import com.eldercare.eldercare.config.ConfiguracaoFirebase;
 import com.eldercare.eldercare.helper.Base64Custom;
 import com.google.firebase.auth.FirebaseAuth;
@@ -12,6 +15,12 @@ import com.google.firebase.database.Exclude;
 import com.google.firebase.database.ValueEventListener;
 
 import java.io.Serializable;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 //implements Serializable serve para passar um contato de uma activity para a outra
 public class Contacto implements Serializable {
@@ -51,6 +60,55 @@ public class Contacto implements Serializable {
 
         }
 
+        /*---------------------------------
+        ---------Mandar notificacao--------
+        ---------------------------------*/
+
+        String baseUrl = "https://fcm.googleapis.com/fcm/";
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        //montar o objeto de notificacao
+
+        DatabaseReference tokenRef = firebaseRef
+                .child("utilizadores")
+                .child(this.idPaciente);
+
+        tokenRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String token = snapshot.child("token").getValue().toString();
+                String to = token;
+                Notificacao notificacao = new Notificacao("ElderCare", "O cuidador acabou de editar um contacto!");
+                NotificacaoDados notificacaoDados = new NotificacaoDados(to, notificacao);
+
+                NotificacaoService service = retrofit.create(NotificacaoService.class);
+                Call<NotificacaoDados> call = service.guardarNotificacao(notificacaoDados);
+                call.enqueue(new Callback<NotificacaoDados>() {
+                    @Override
+                    public void onResponse(Call<NotificacaoDados> call, Response<NotificacaoDados> response) {
+                        if(response.isSuccessful()){
+
+                            Log.i("codigo", "codigo: " + response.code());
+
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<NotificacaoDados> call, Throwable t) {
+
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 
     public void guardar(){
@@ -70,6 +128,55 @@ public class Contacto implements Serializable {
                 .child(this.idPaciente)
                 .child(idContacto)
                 .setValue(this);
+
+        /*---------------------------------
+        ---------Mandar notificacao--------
+        ---------------------------------*/
+
+        String baseUrl = "https://fcm.googleapis.com/fcm/";
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        //montar o objeto de notificacao
+
+        DatabaseReference tokenRef = firebaseRef
+                .child("utilizadores")
+                .child(this.idPaciente);
+
+        tokenRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String token = snapshot.child("token").getValue().toString();
+                String to = token;
+                Notificacao notificacao = new Notificacao("ElderCare", "O cuidador acabou de adicionar um contacto!");
+                NotificacaoDados notificacaoDados = new NotificacaoDados(to, notificacao);
+
+                NotificacaoService service = retrofit.create(NotificacaoService.class);
+                Call<NotificacaoDados> call = service.guardarNotificacao(notificacaoDados);
+                call.enqueue(new Callback<NotificacaoDados>() {
+                    @Override
+                    public void onResponse(Call<NotificacaoDados> call, Response<NotificacaoDados> response) {
+                        if(response.isSuccessful()){
+
+                            Log.i("codigo", "codigo: " + response.code());
+
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<NotificacaoDados> call, Throwable t) {
+
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
     }
 
--- app/src/main/java/com/eldercare/eldercare/model/Lembrete.java
+++ app/src/main/java/com/eldercare/eldercare/model/Lembrete.java
@@ -1,12 +1,26 @@
 package com.eldercare.eldercare.model;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.eldercare.eldercare.api.NotificacaoService;
 import com.eldercare.eldercare.config.ConfiguracaoFirebase;
 import com.eldercare.eldercare.helper.Base64Custom;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.Exclude;
+import com.google.firebase.database.ValueEventListener;
 
 import java.io.Serializable;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Lembrete implements Serializable {
 
@@ -39,6 +53,55 @@ public class Lembrete implements Serializable {
                 .child(idLembrete)
                 .setValue(this);
 
+        /*---------------------------------
+        ---------Mandar notificacao--------
+        ---------------------------------*/
+
+        String baseUrl = "https://fcm.googleapis.com/fcm/";
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        //montar o objeto de notificacao
+
+        DatabaseReference tokenRef = firebaseRef
+                .child("utilizadores")
+                .child(this.idPaciente);
+
+        tokenRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String token = snapshot.child("token").getValue().toString();
+                String to = token;
+                Notificacao notificacao = new Notificacao("ElderCare", "O cuidador acabou de adicionar um lembrete!");
+                NotificacaoDados notificacaoDados = new NotificacaoDados(to, notificacao);
+
+                NotificacaoService service = retrofit.create(NotificacaoService.class);
+                Call<NotificacaoDados> call = service.guardarNotificacao(notificacaoDados);
+                call.enqueue(new Callback<NotificacaoDados>() {
+                    @Override
+                    public void onResponse(Call<NotificacaoDados> call, Response<NotificacaoDados> response) {
+                        if(response.isSuccessful()){
+
+                            Log.i("codigo", "codigo: " + response.code());
+
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<NotificacaoDados> call, Throwable t) {
+
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 
     public void editar(){
@@ -65,6 +128,55 @@ public class Lembrete implements Serializable {
                     .child(this.getKey()).removeValue();
 
         }
+
+        /*---------------------------------
+        ---------Mandar notificacao--------
+        ---------------------------------*/
+
+        String baseUrl = "https://fcm.googleapis.com/fcm/";
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        //montar o objeto de notificacao
+
+        DatabaseReference tokenRef = firebaseRef
+                .child("utilizadores")
+                .child(this.idPaciente);
+
+        tokenRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String token = snapshot.child("token").getValue().toString();
+                String to = token;
+                Notificacao notificacao = new Notificacao("ElderCare", "O cuidador acabou de editar um lembrete!");
+                NotificacaoDados notificacaoDados = new NotificacaoDados(to, notificacao);
+
+                NotificacaoService service = retrofit.create(NotificacaoService.class);
+                Call<NotificacaoDados> call = service.guardarNotificacao(notificacaoDados);
+                call.enqueue(new Callback<NotificacaoDados>() {
+                    @Override
+                    public void onResponse(Call<NotificacaoDados> call, Response<NotificacaoDados> response) {
+                        if(response.isSuccessful()){
+
+                            Log.i("codigo", "codigo: " + response.code());
+
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<NotificacaoDados> call, Throwable t) {
+
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
     }
 
